d8cf20460f7492198c5ea35b41ed81fb5c5260a7
mel239 <melania231992@gmail.com>
2023-02-23 11:23:30 +0100

    animation

--- src/pages/Landpage.jsx
+++ src/pages/Landpage.jsx
@@ -4,8 +4,8 @@ import AnchorLink from 'react-anchor-link-smooth-scroll';
 export default function Landpage() {
 	return (
 		<div className='min-h-screen flex justify-center items-center'>
-			<div className='text-center'>
-				<h1 className='my-5'>Melania</h1>
+			<div className='text-center animate-fade-in-down'>
+				<h1 className='my-5 '>Melania</h1>
 				<p className='mx-20 mb-10'>
 					I like to design and structure Web Apps, developing features to
 					enhance the use's experience while striking a balance between
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,7 +1,23 @@
 module.exports = {
 	content: ['./index.html', './src/**/*.{js,jsx}'],
 	theme: {
-		extend: {},
+		extend: {
+			keyframes: {
+				'fade-in-down': {
+					'0%': {
+						opacity: '0',
+						transform: 'translateY(-15px)',
+					},
+					'100%': {
+						opacity: '1',
+						transform: 'translateY(0)',
+					},
+				},
+			},
+			animation: {
+				'fade-in-down': 'fade-in-down 1s ease-out',
+			},
+		},
 	},
 	plugins: [],
 };
